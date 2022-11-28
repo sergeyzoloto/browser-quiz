f021fa6f856fa68a8399ce21e1a7c59a8a42421c
Sergey Zolotko <sergiozoloto@gmail.com>
2022-11-28 14:12:21 +0100

    Refactored the answerlist into a bunch of buttons + css paddings

--- src/views/answerView.js
+++ src/views/answerView.js
@@ -3,12 +3,10 @@
  * @returns {Element}
  */
 export const createAnswerElement = (key, answerText) => {
-  const element = document.createElement('li');
   const button = document.createElement('button');
-  element.appendChild(button);
-  button.className = String.raw`${key} ans-btn`;
+  button.className = `${key} ans-btn`;
   button.innerHTML = String.raw`
     ${key}: ${answerText};
   `;
-  return element;
+  return button;
 };
