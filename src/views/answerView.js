e63d8860e7b96ff61b17bfde21b83d70d94eeb8a
Sergey Zolotko <sergiozoloto@gmail.com>
2022-12-01 16:58:23 +0100

    Checkmarks reworked and crosses added

--- src/views/answerView.js
+++ src/views/answerView.js
@@ -7,7 +7,8 @@ export const createAnswerElement = (key, answerText) => {
   button.classList.add(`ans-btn`);
   button.innerHTML = String.raw`
     <div class="letter"><p>${key.toUpperCase()}</p></div><div class="answer">${answerText}</div>
-    <svg class="checkmark"><path d="M14.293.293l1.414 1.414L5 12.414.293 7.707l1.414-1.414L5 9.586z"></path></svg>
+    <div class="checkmarkV">&checkmark;</div>
+    <div class="checkmarkX">X</div>
   `;
 
   return button;
